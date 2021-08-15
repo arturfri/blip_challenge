e03c4444ddd2b3345cb627b6b31dd0d7f902f81f
Artur Felipe Ribeiro <artur.felipe.ribeiro2010@gmail.com>
2021-08-15 20:27:48 -0300

    move api to folder and add mybotflow

--- Api/index.js
+++ Api/index.js
@@ -9,10 +9,6 @@ app.get('/', (req, res) => res.send('Welcome!'))
 app.get('/github', async(req, res) => {
     try {
       const response = await axios.get('https://api.github.com/orgs/takenet/repos?direction=asc')
-      // console.log(Array.isArray(response.data))
-      // const topics = await axios.get(`https://api.github.com/repos/takenet/${response.data[0].name}/languages`,{headers: {
-      //   "Accept": "application/vnd.github.v3+json"
-      // }})
       const formatedValues = {}
       response.data.filter(r => r.language === 'C#').slice(0, 5).forEach((item, index) => {
         formatedValues[index] = {
